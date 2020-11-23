31a8cffc806d73c3cacdb330df32a809cc56267a
Iakovos Vogiatzoglou <9253011+iakovosvo@users.noreply.github.com>
2020-11-23 16:15:48 +0100

    initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,24 @@
+module.exports = {
+  'env': {
+    'browser': true,
+    'es6': true,
+    'node': true
+  },
+  'extends': 'eslint:recommended',
+  'globals': {
+    'Atomics': 'readonly',
+    'SharedArrayBuffer': 'readonly'
+  },
+  'parserOptions': {
+    'ecmaVersion': 2018,
+    'sourceType': 'module'
+  },
+  'rules': {
+    'quotes': ['error', 'single'],
+    'semi': ['error', 'always'],
+    'indent': ['error', 2],
+    'no-multi-spaces': ['error'],
+    'no-trailing-spaces': ['error'],
+    'no-multiple-empty-lines': ['error', { 'max': 1, 'maxEOF': 0 }]
+  }
+};
